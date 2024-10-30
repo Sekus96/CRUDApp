87504386c9aa85c5300e36c86e36608ad87611c8
Sekus96 <radsek.96@gmail.com>
2024-10-30 13:03:28 +0100

    Little update on validations messages & GlobalExceptionHandler

--- src/main/java/com/example/CRUDApp/dto/RegisterDto.java
+++ src/main/java/com/example/CRUDApp/dto/RegisterDto.java
@@ -3,21 +3,23 @@ package com.example.CRUDApp.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class RegisterDto {
     @NotBlank(message = "Username is required.")
     @Pattern(
-            regexp = "^[a-zA-Z0-9]{3,20}$",
+            regexp = "^[a-zA-Z0-9]{4,20}$",
             message = "Login must contain at least 4 characters and not more than 20 characters."
     )
     @Schema(example = "enter_your_login")
     private String username;
     @NotBlank(message = "Password is required.")
+    @Size(min = 8, message = "Password must contain at least 8 characters.")
     @Pattern(
-            regexp = "^(?=.*[0-9])(?=.*[A-Z])(?=.*[@#$%^&+=!.])(?=\\S+$).{8,}$",
-            message = "Password must contain at least 8 characters, including one uppercase letter, one digit, and one special character."
+            regexp = "^(?=.*[0-9])(?=.*[A-Z])(?=.*[@#$%^&+=!.]).+$",
+            message = "Password must contain at least one uppercase letter, one digit, and one special character."
     )
     @Schema(example = "enter_your_password")
     private String password;
--- src/main/java/com/example/CRUDApp/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/example/CRUDApp/exceptions/GlobalExceptionHandler.java
@@ -5,18 +5,25 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.validation.FieldError;
+
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
-        Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getFieldErrors().forEach(error ->
-                errors.put(error.getField(), error.getDefaultMessage())
-        );
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+    public ResponseEntity<Map<String, List<String>>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, List<String>> errors = new HashMap<>();
+
+        for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+            String fieldName = error.getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.computeIfAbsent(fieldName, key -> new ArrayList<>()).add(errorMessage);
+        }
+        return ResponseEntity.badRequest().body(errors);
     }
 }
