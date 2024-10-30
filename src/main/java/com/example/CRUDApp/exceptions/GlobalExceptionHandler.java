caf1205b78c0d6ae2f2aff2ac46d7b3bb36c1175
Sekus96 <radsek.96@gmail.com>
2024-10-30 13:48:39 +0100

    Updating GlobalExceptionHandler

--- src/main/java/com/example/CRUDApp/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/example/CRUDApp/exceptions/GlobalExceptionHandler.java
@@ -1,7 +1,10 @@
 package com.example.CRUDApp.exceptions;
 
+import org.springdoc.api.ErrorMessage;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -25,5 +28,16 @@ public class GlobalExceptionHandler {
             errors.computeIfAbsent(fieldName, key -> new ArrayList<>()).add(errorMessage);
         }
         return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(InternalAuthenticationServiceException.class)
+    public ResponseEntity<String> handleInternalAuthenticationServiceException(InternalAuthenticationServiceException e) {
+        return new ResponseEntity<String>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(BadCredentialsException.class) // when Invalid Credentials
+    public ResponseEntity<String> handleInvalidCredentialsException(
+            BadCredentialsException ex) {
+        return new ResponseEntity<String>("Podane hasło jest nieprawidłowe.", HttpStatus.UNAUTHORIZED);
     }
 }
